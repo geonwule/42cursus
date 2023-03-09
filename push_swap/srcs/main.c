6497ee083dc9635d02fe8923852f4150c524989f
Geonwu Lee <geonwlee22@gmail.com>
2023-03-09 20:41:40 +0900

    ing

--- push_swap/srcs/main.c
+++ push_swap/srcs/main.c
@@ -6,84 +6,11 @@
 /*   By: geonwule <geonwule@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/08 22:13:49 by geonwule          #+#    #+#             */
-/*   Updated: 2023/03/09 20:07:13 by geonwule         ###   ########.fr       */
+/*   Updated: 2023/03/09 20:41:24 by geonwule         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
-
-void	a_to_b(int size, t_list **node_a, t_list **node_b, t_list **order)
-{
-	long long	x;
-	long long	y;
-
-	if (size <= 2)
-	{
-		a_to_b_f(size, node_a, node_b, order);
-		return ;
-	}
-	long long pivot, ra_c = 0, pb_c = 0;
-	x = (*node_a)->content;
-	y = (*node_a)->next->content;
-	pivot = ((x + y) / 2);
-	if (pivot < 0 || (pivot == 0 && (x < 0 || y < 0)))
-		pivot -= 1;
-	for (int i = 0; i < size; i++)
-	{
-		if ((*node_a)->content > pivot)
-		{
-			ft_ra(node_a, order, 1);
-			ra_c++;
-		}
-		else
-		{
-			ft_pb(node_a, node_b, order);
-			pb_c++;
-		}
-	}
-	for (int i = 0; i < ra_c; i++)
-		ft_rra(node_a, order, 1);
-	a_to_b(ra_c, node_a, node_b, order);
-	b_to_a(pb_c, node_a, node_b, order);
-}
-
-void	b_to_a(int size, t_list **node_a, t_list **node_b, t_list **order)
-{
-	long long	x;
-	long long	y;
-
-	if (size <= 2)
-	{
-		b_to_a_f(size, node_a, node_b, order);
-		return ;
-	}
-	long long pivot, rb_c = 0, pa_c = 0;
-	x = (*node_b)->content;
-	y = (*node_b)->next->content;
-	pivot = ((x + y) / 2);
-	if (pivot < 0 || (pivot == 0 && (x < 0 || y < 0)))
-		pivot -= 1;
-	pivot = (((*node_b)->content + (*node_b)->next->content) / 2);
-	if (pivot <= 0)
-		pivot -= 1;
-	for (int i = 0; i < size; i++)
-	{
-		if ((*node_b)->content <= pivot)
-		{
-			ft_rb(node_b, order, 1);
-			rb_c++;
-		}
-		else
-		{
-			ft_pa(node_a, node_b, order);
-			pa_c++;
-		}
-	}
-	for (int i = 0; i < rb_c; i++)
-		ft_rrb(node_b, order, 1);
-	a_to_b(pa_c, node_a, node_b, order);
-	b_to_a(rb_c, node_a, node_b, order);
-}
 
 int	main(int ac, char **av)
 {
